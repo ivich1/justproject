8aeab901768bb9e3eab4c5c3cbe527ed2737ff5c
ivich1 <ivan.chiernighin@mail.ru>
2022-04-01 10:34:15 +0300

    починил обход с перпядствиями, добавил трек путей

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import functions.StringProcesor;
-import functions.StringToNumberAdder;
 import robotProgram.RobotPathFinder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -9,17 +10,53 @@ public class Main {
         //пока что так
         //0 - свободно
         //1 - препядствие
-        // старт и финиш - углы
+        //старт и финиш - углы
+        //все поля пеервернутые) те старт находится в 00(слева сверху), финиш в конце(справа внизу)
         int [][] testField1 = {
+                {0,0,0},
+                {0,1,1},
+                {0,0,0}
+        };//у данного поля 1
+
+        int [][] testField2 = {
                 {0,0,0},
                 {0,1,0},
                 {0,0,0}
-        };//у данного поля 2 пути
+        };//у данного поля 2
 
-        RobotPathFinder r = new RobotPathFinder(testField1);
+        int [][] testField3 = {
+                {0,0,0},
+                {0,0,0},
+                {0,0,0}
+        };//у данного поля 6
+
+        int [][] testField0 = {
+                {0,0,0},
+                {0,1,1},
+                {0,0,1}
+        };//0
+
+
+        int [][] testField4 = {
+                {0,0,0,0},
+                {0,1,0,0},
+                {0,0,0,1},
+                {0,0,0,0}
+        };//4
+
+        int [][] testField5 = {
+                {0,0,0},
+                {0,1,0},
+                {0,0,0},
+                {0,0,0}
+        };//4
+
+        RobotPathFinder r = new RobotPathFinder(testField0);
 
         //System.out.println(r.findNumberEmptyField(3,3));
-        System.out.println(r.findNumberEmptyField());
+        System.out.println(r.findNumberWaysSimpleCase());
+        r.PrintWays();
 
+        LinkedList<String> a = new LinkedList<>();
     }
 }
--- src/robotProgram/RobotPathFinder.java
+++ src/robotProgram/RobotPathFinder.java
@@ -1,9 +1,13 @@
 package robotProgram;
 
+import java.awt.*;
+import java.util.LinkedList;
+
 public class RobotPathFinder {
 
     private int globalWayCounter;
     private int[][] field;
+    private LinkedList<LinkedList<Point>> WayList;
 
     public RobotPathFinder(){
         globalWayCounter = 0;
@@ -11,58 +15,63 @@ public class RobotPathFinder {
 
     public RobotPathFinder(int [][] field){
         this.field = field;
+        WayList = new LinkedList<>();
     }
 
     //поиск пути рекурсивно
     //простая функция поиска решения? опирается на доп функции
     //!!!w, h начинаятся с 1!!! - не знаю, верно ли
-    public int findNumberEmptyField(int width, int height){
-        int res =recursiveFindWayPossibilities(width,height,0);
+    public int findNumberWaysSimpleCase(int width, int height){
+        int res =recursiveFindWayPossibilities(width,height, 0);
         return res;
     }
+
     //перегрузка пердыдущей для текущего поля
-    public int findNumberEmptyField(){
-        if(field ==null){
+    public int findNumberWaysSimpleCase(){
+        if(field == null){
             return 0;
         }
-        int res =recursiveFindWayWithBarriers(field.length,field[0].length,0);
+        if (!checkCanMovePossibility(field.length - 1,field[0].length - 1)){
+            return 0;
+        }
+        int res =recursiveFindWayWithBarriers(field.length,field[0].length,new LinkedList<Point>());
         return res;
     }
 
     //рекурсивно ищет все маршруты не запоминая их
     //ротатет как обход графа вглубину(вроде бы)
-    private int recursiveFindWayPossibilities(int wPosition, int hPosition, int currentNumberOfWays){
+    private int recursiveFindWayPossibilities(int wPosition, int hPosition, int currentNumberOfWays ){
+
         if(wPosition == 1 && hPosition == 1){
             globalWayCounter++;
             return 1;
         }else{
             if(wPosition != 1){
-                currentNumberOfWays += 1;
-                currentNumberOfWays += recursiveFindWayPossibilities(wPosition -1, hPosition, currentNumberOfWays);
+                recursiveFindWayPossibilities(wPosition -1, hPosition, currentNumberOfWays);
             }
             if(hPosition != 1){
-                currentNumberOfWays += 1;
                 recursiveFindWayPossibilities(wPosition, hPosition -1, currentNumberOfWays);
             }
         }
         return globalWayCounter;
     }
 
-    //рекурсивно ищет все маршруты не запоминая их
+    //рекурсивно ищет все маршруты
     //работает с полем, на котором есть перпядствия
     //тот же обход графа, но при препядствии не вызывает саму себя
-    private int recursiveFindWayWithBarriers(int wPosition, int hPosition, int currentNumberOfWays){
+    private int recursiveFindWayWithBarriers(int wPosition, int hPosition, LinkedList<Point> way){
+        LinkedList<Point> currentWay = new LinkedList<>(way);
+        currentWay.addFirst(new Point(wPosition - 1, hPosition - 1));
         if(wPosition == 1 && hPosition == 1){
             globalWayCounter++;
+            WayList.add(currentWay);
             return 1;
         }else{
-            if(wPosition != 1 && checkCanMovePossibility(wPosition - 1 - 1, hPosition - 1)){
-                currentNumberOfWays += 1;
-                currentNumberOfWays += recursiveFindWayPossibilities(wPosition -1, hPosition, currentNumberOfWays);
+            if(wPosition != 1 && checkCanMovePossibility(wPosition - 2, hPosition - 1)){
+                recursiveFindWayWithBarriers(wPosition -1, hPosition, currentWay);
             }
             if(hPosition != 1 && checkCanMovePossibility(wPosition - 1, hPosition - 1 - 1)){
-                currentNumberOfWays += 1;
-                recursiveFindWayPossibilities(wPosition, hPosition -1, currentNumberOfWays);
+                recursiveFindWayWithBarriers(wPosition, hPosition -1, currentWay);
             }
         }
         return globalWayCounter;
@@ -71,6 +80,17 @@ public class RobotPathFinder {
     //возвращает возможно ли возможно ли наступить на клетку
     private boolean checkCanMovePossibility(int x, int y){
         return field[x][y] == 0;
+    }
+
+    //печатает список путей
+    public void PrintWays(){
+        for (var list: WayList) {
+            for (var point: list) {
+                System.out.print(point.x +"-"+point.y);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
 
